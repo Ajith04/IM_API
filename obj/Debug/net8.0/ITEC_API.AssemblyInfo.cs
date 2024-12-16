a5c29baabb67bba15813a8d42ee6f9995b0ed371
Ajith Sritharan <sritharanajith04@gmail.com>
2024-12-17 01:13:10 +0530

    finnnal

--- obj/Debug/net8.0/ITEC_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITEC_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITEC_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f448d804d3edfab2fda1c2bdf9e195c294367e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca2c8176a35d580e29e6312f83ae6b9f815fa15")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITEC_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITEC_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
