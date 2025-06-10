5095a0b3324687bfc89ecf2c75028abd74ff7505
Ajith UCA <ajith@unicornconnectedapps.com>
2025-06-10 17:37:30 +0530

    w12

--- obj/Debug/net8.0/ITEC_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ITEC_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITEC_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccdaf1ec2c4edb3704e5e3bc8552df81fce6a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249ff86882cd928571aecbf4f8c01721d246fe73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITEC_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITEC_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
